4b07c774141fe3fd410b189bcb34750efda21b90
zdulak <60813197+zdulak@users.noreply.github.com>
2022-04-11 22:49:56 +0200

    Delete extends App for Isogram

--- src/main/scala/Isogram.scala
+++ src/main/scala/Isogram.scala
@@ -1,4 +1,4 @@
-object Isogram extends App {
+object Isogram {
   def isIsogram(word: String): Boolean = {
     word.groupBy(identity).forall { case (c, str) => if (c != ' ' && c != '-') str.length == 1 else true }
   }
